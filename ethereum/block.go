a70f6257f7febf1f5a6662a10af90fe6f3383cd8
Muzry <danli@numencyber.com>
2023-03-16 11:28:58 +0800

    fix: retry when getting latest block occurs err

--- ethereum/block.go
+++ ethereum/block.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"log"
 	"math/big"
+	"time"
 
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/sirupsen/logrus"
 
 	"go-etl/client"
 	"go-etl/config"
@@ -19,35 +21,43 @@ func GetLatestBlocks() chan types.Block {
 }
 
 func getLatestBlocks(blocks chan types.Block) {
-	lastBlockNumber := utils.ReadBlockNumberFromFile(config.Conf.ETLConfig.PreviousFile)
-
-	headers := make(chan *types.Header, 10)
-	sub, err := client.EvmClient().SubscribeNewHead(context.Background(), headers)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	for {
-		select {
-		case err := <-sub.Err():
-			log.Fatal(err)
-		case header := <-headers:
-			if lastBlockNumber != 0 {
-				for lastBlockNumber+1 < header.Number.Int64() {
-					block, err := client.EvmClient().BlockByNumber(context.Background(), big.NewInt(lastBlockNumber+1))
-					if err != nil {
-						log.Fatal(err)
+		lastBlockNumber := utils.ReadBlockNumberFromFile(config.Conf.ETLConfig.PreviousFile) + 1
+
+		headers := make(chan *types.Header, 10)
+		sub, err := client.EvmClient().SubscribeNewHead(context.Background(), headers)
+		if err != nil {
+			logrus.Infof("failed to subscribe to new head block: %v, retry after 5s", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		logrus.Infof("subscribed to new head")
+
+		for {
+			select {
+			case err = <-sub.Err():
+				logrus.Errorf("subscription error: %v\n", err)
+				sub.Unsubscribe()
+				break
+			case header := <-headers:
+				if lastBlockNumber != 0 {
+					for lastBlockNumber < header.Number.Int64() {
+						block, err := client.EvmClient().BlockByNumber(context.Background(), big.NewInt(lastBlockNumber))
+						if err != nil {
+							log.Fatal(err)
+						}
+						blocks <- *block
+						lastBlockNumber = lastBlockNumber + 1
 					}
-					blocks <- *block
-					lastBlockNumber = lastBlockNumber + 1
 				}
+				block, err := client.EvmClient().BlockByHash(context.Background(), header.Hash())
+				if err != nil {
+					logrus.Fatalf("get the block by hash %s is err %v", block.Hash(), err)
+				}
+				blocks <- *block
+				lastBlockNumber = header.Number.Int64()
 			}
-			block, err := client.EvmClient().BlockByHash(context.Background(), header.Hash())
-			if err != nil {
-				log.Fatal(err)
-			}
-			blocks <- *block
-			lastBlockNumber = header.Number.Int64()
 		}
 	}
 }
