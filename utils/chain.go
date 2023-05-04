287af6a0b4759a99668bdd1874d178dae5713a88
Muzry <danli@numencyber.com>
2023-05-04 10:49:48 +0800

    update: rename the chain

--- utils/chain.go
+++ utils/chain.go
@@ -1,9 +1,27 @@
 package utils
 
+const (
+	ChainEthereum  = "ethereum"
+	ChainEth       = "eth"
+	ChainBSC       = "bsc"
+	ChainEmpty     = ""
+	ChainKey       = "chain"
+	ChainOptimism  = "optimism"
+	ChainFantom    = "fantom"
+	ChainArbitrum  = "arbitrum"
+	ChainAvalanche = "avalanche"
+	ChainPolygon   = "polygon"
+	ChainCelo      = "celo"
+)
+
 func GetChainFromQuery(chain string) string {
 	switch chain {
 	case ChainEmpty:
 		return ChainEthereum
+	case ChainEth, ChainEthereum:
+		return ChainEthereum
+	case ChainBSC:
+		return ChainBSC
 	default:
 		return chain
 	}
@@ -11,11 +29,22 @@ func GetChainFromQuery(chain string) string {
 
 func ConvertChainToMetaDock(chain string) string {
 	switch chain {
-	case ChainEthereum:
-		return "eth"
+	case ChainEthereum, ChainEth:
+		return ChainEth
 	case ChainBSC:
-		return "bsc"
+		return ChainBSC
 	default:
-		return "eth"
+		return ChainEth
+	}
+}
+
+func ConvertChainToDeFiHackLabChain(chain string) string {
+	switch chain {
+	case ChainEthereum, ChainEth:
+		return ChainEth
+	case ChainBSC:
+		return ChainBSC
+	default:
+		return ChainEth
 	}
 }
