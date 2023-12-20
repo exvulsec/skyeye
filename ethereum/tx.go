5b18bc5c659bb912ae737e52020756b220d2568a
Muzry <danli@numencyber.com>
2023-12-20 18:54:30 +0800

    update: add timeout context for get block

--- ethereum/tx.go
+++ ethereum/tx.go
@@ -49,25 +49,24 @@ func (te *transactionExecutor) Run() {
 	if te.logExecutor != nil {
 		go te.logExecutor.Run()
 	}
-
+	timeoutContext, _ := context.WithTimeout(context.Background(), 10)
 	for blockNumber := range te.blockExecutor.blocks {
-		block, err := client.EvmClient().BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
+		block, err := client.EvmClient().BlockByNumber(timeoutContext, big.NewInt(int64(blockNumber)))
 		if err != nil {
-			if strings.Contains(err.Error(), "not found") {
-				timeoutContext, _ := context.WithTimeout(context.Background(), 3)
+			if strings.Contains(err.Error(), "not found") || strings.Contains(err.Error(), "context deadline exceeded") {
 				for index := 0; index < 3; index++ {
 					time.Sleep(1 * time.Second)
 					logrus.Infof("retry %d to get block: %d info", index+1, blockNumber)
 					block, err = client.EvmClient().BlockByNumber(timeoutContext, big.NewInt(int64(blockNumber)))
-					if err != nil && !strings.Contains(err.Error(), "not found") {
-						logrus.Fatalf("get block from raw json message is err: %v, item is %+v", err, blockNumber)
+					if err != nil && (!strings.Contains(err.Error(), "not found") && !strings.Contains(err.Error(), "context deadline exceeded")) {
+						logrus.Errorf("get block %d info is err: %v, drop it ", blockNumber, err)
 					}
 					if block != nil {
 						break
 					}
 				}
 			} else {
-				logrus.Fatalf("get block from raw json message is err: %v, item is %+v", err, blockNumber)
+				logrus.Errorf("get block %d info is err: %v, drop it ", blockNumber, err)
 			}
 		}
 		logrus.Infof("start to extract transaction infos from the block: %d infos", blockNumber)
