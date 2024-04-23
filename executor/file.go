437d41a817c0b97d62108508ec77162d4cc2e832
Muzry <muzrry@gmail.com>
2024-04-23 17:50:22 +0800

    fix: file executor to write blocknumber if it is 0 (#39)

--- executor/file.go
+++ executor/file.go
@@ -8,22 +8,22 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/exvulsec/skyeye/config"
-	"github.com/exvulsec/skyeye/utils"
 )
 
 type fileExecutor struct {
-	items             chan any
-	latestBlockNumber int64
-	waitList          []int64
-	filePath          string
+	items               chan any
+	latestBlockNumberCh chan int64
+	latestBlockNumber   int64
+	waitList            []int64
+	filePath            string
 }
 
-func NewFileExecutor() Executor {
+func NewFileExecutor(latestBlockNumberCh chan int64) Executor {
 	return &fileExecutor{
-		items:             make(chan any, 10),
-		filePath:          config.Conf.ETL.PreviousFile,
-		waitList:          []int64{},
-		latestBlockNumber: int64(utils.GetBlockNumberFromFile(config.Conf.ETL.PreviousFile)),
+		items:               make(chan any, 10),
+		filePath:            config.Conf.ETL.PreviousFile,
+		latestBlockNumberCh: latestBlockNumberCh,
+		waitList:            []int64{},
 	}
 }
 
@@ -36,20 +36,24 @@ func (fe *fileExecutor) GetItemsCh() chan any {
 }
 
 func (fe *fileExecutor) Execute() {
-	for item := range fe.items {
-		blockNumber := item.(int64)
-		if fe.writeBlock(blockNumber) {
-			for index, waitBlock := range fe.waitList {
-				if !fe.writeBlock(waitBlock) {
-					fe.waitList = fe.waitList[index:]
-					break
+	select {
+	case v := <-fe.latestBlockNumberCh:
+		fe.latestBlockNumber = v
+		for item := range fe.items {
+			blockNumber := item.(int64)
+			if fe.writeBlock(blockNumber) {
+				for index, waitBlock := range fe.waitList {
+					if !fe.writeBlock(waitBlock) {
+						fe.waitList = fe.waitList[index:]
+						break
+					}
 				}
+			} else {
+				fe.waitList = append(fe.waitList, blockNumber)
+				sort.SliceStable(fe.waitList, func(i, j int) bool {
+					return fe.waitList[i] < fe.waitList[j]
+				})
 			}
-		} else {
-			fe.waitList = append(fe.waitList, blockNumber)
-			sort.SliceStable(fe.waitList, func(i, j int) bool {
-				return fe.waitList[i] < fe.waitList[j]
-			})
 		}
 	}
 }
