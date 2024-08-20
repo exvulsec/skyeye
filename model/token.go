89d206822074c8a8e16e2c9e5ca066dfbb2586b7
Muzry <muzrry@gmail.com>
2024-08-20 15:12:58 +0800

    update: keep 2 round with decimals

--- model/token.go
+++ model/token.go
@@ -94,7 +94,7 @@ func (t *Token) GetSymbol() string {
 
 func (t *Token) GetValueWithDecimals(value decimal.Decimal) decimal.Decimal {
 	pow := decimal.NewFromInt(10).Pow(decimal.NewFromInt(t.Decimals))
-	return value.DivRound(pow, 8)
+	return value.DivRound(pow, 2)
 }
 
 func (t *Token) GetValueWithDecimalsAndSymbol(value decimal.Decimal) string {
